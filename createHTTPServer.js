b9fa7208f1c17c6ee2b96b448177d92e64e10447
Arun Nair <ariesaru2002@gmail.com>
2014-09-27 04:13:30 -0400

    Made second change to ma app

--- createHTTPServer.js
+++ createHTTPServer.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.set('ipaddress', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);
 
 app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
