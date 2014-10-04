10be8ca8b63f5f6780d077deb1f5d5d225900945
Arun Nair <ariesaru2002@gmail.com>
2014-10-03 20:58:48 -0400

    Added path to html page

--- routes/serveURLs.js
+++ routes/serveURLs.js
@@ -5,7 +5,7 @@ var serveLogin = function (req, res) {
 
 var serveIndex = function (req, res) {
 	console.log("Serving index!");
-	res.send('./public/BasicEmployeeProject.html');
+	res.sendfile('./public/BasicEmployeeProject.html');
 }
 
 var serveIndexNumber = function (req, res) {
