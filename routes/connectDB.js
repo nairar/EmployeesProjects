f795adf88317cefe56cdd9d0625464f68d72e038
Arun Nair <ariesaru2002@gmail.com>
2014-10-04 06:00:53 -0400

    Second project 'Employee Page' again added

--- routes/connectDB.js
+++ routes/connectDB.js
@@ -1,13 +1,9 @@
 ﻿var mongo = require("mongojs");
-var dbUrl = 'mongodb://localhost:27018/ArunNair'
-
-if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-    db_url = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+var db_url = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
     process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
     process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
     process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
     process.env.OPENSHIFT_APP_NAME;
-}
 
 var connection = mongo(dbUrl, ['Emp']);
 
